ed0e3f1fda78f12b9a7103d55d3801393f517b65
David Nicholson <nickledave@users.noreply.github.com>
2024-04-20 21:09:37 -0400

    Update src/biosoundsegbench/prep/copy_audio_copy_make_annot.py

--- src/biosoundsegbench/prep/copy_audio_copy_make_annot.py
+++ src/biosoundsegbench/prep/copy_audio_copy_make_annot.py
@@ -7,6 +7,7 @@ generate placeholder annotations if we need to.
 """
 import logging
 import shutil
+from collections import defaultdict
 
 import crowsetta
 import numpy as np
@@ -127,7 +128,7 @@ def copy_wav_convert_annot_birdsongrec(dry_run=True):
             )
         yarden = YARDEN_SCRIBE.from_file(annot_mat)
         annots = []
-        pbar = tqdm(
+        pbar = tqdm.tqdm(
             zip(yarden.annotations, yarden.audio_paths)
         )
         for filenum, (annot_arr, audio_path) in enumerate(pbar):
@@ -173,7 +174,13 @@ GENERIC_SEQ_SCRIBE = crowsetta.Transcriber(format='generic-seq')
 
 
 def copy_wav_convert_annot_tweetynet_canary(dry_run=True):
-    """"""
+    """Copy audio files and convert annotation files canary song dataset from
+    TweetyNet paper.
+
+    We convert the annotation files from the 'generic-seq' format
+    (a single annotation file contains annotations for multiple audio files)
+    to the 'simple-seq' format (a single annotation file contains only
+    annotations for a single audio file)"""
     for bird_id in constants.TWEETYNET_BIRD_IDS:
         logger.info(
             f"Copying audio files for bird ID {bird_id} from TweetyNet dataset"
@@ -200,7 +207,8 @@ def copy_wav_convert_annot_tweetynet_canary(dry_run=True):
 
 
     # Make annotation files.
-    # We convert from `"generic-seq"` (single file for all annotations) to `"simple-seq"` (one annotation file per one audio file)
+    # We convert from `"generic-seq"` (single file for all annotations)
+    # to `"simple-seq"` (one annotation file per one audio file)
     for bird_id in constants.TWEETYNET_BIRD_IDS:
         logger.info(
             f"Making annotation files for bird ID {bird_id} from TweetyNet dataset"
@@ -247,7 +255,8 @@ def copy_wav_convert_annot_tweetynet_canary(dry_run=True):
     # We do the clean up step of removing audio files with annotations here.
     for bird_id in constants.TWEETYNET_BIRD_IDS:
         logger.info(
-            f"Removing audio files without an annotation file for bird ID {bird_id} from Birdsong-Recognition dataset."
+            f"Removing audio files without an annotation file for bird ID {bird_id} "
+            "from Birdsong-Recognition dataset."
         )
 
         bird_id_dst = constants.CANARY_DATA_DST / f"tweetynet-{bird_id}"
@@ -272,6 +281,236 @@ def copy_wav_convert_annot_tweetynet_canary(dry_run=True):
                 wav_path.unlink()
 
 
+def copy_wav_generate_annot_jourjine_et_al_2023(dry_run=True):
+    """Copy wav files and generate annotation files for Jourjine et al. 2023 dataset
+
+    For this dataset we start with the segmentation files,
+    since there are less of those than audio files,
+    and since we generate annotations from the segmentation on the fly.
+    We generate the annotations byh treating this as a binary classification task:
+    segments (above threshold) are labeled "vocalization",
+    all other periods are labeled "no vocalization".
+    So we just label all the detected segments as "v" (for "vocalization")"""
+    segs_csv_path = constants.JOURJINE_ET_AL_2023_SEGS_DIR / "all_development_vocs_with_start_stop_times.csv"
+    segs_df = pd.read_csv(segs_csv_path)
+
+    pbar = tqdm.tqdm(segs_df.source_file.unique())
+    for source_file in pbar:
+        pbar.set_description(
+            f"Processing source file: {source_file}"
+        )
+        this_file_segs_df = segs_df[
+            segs_df.source_file == source_file
+        ]
+        if len(this_file_segs_df) > 0:
+            species_id = source_file.split('_')[0]
+            species_id_dst = constants.MOUSE_PUP_CALL_DATA_DST / f"jourjine-et-al-2023-{species_id}"
+            if not dry_run:
+                species_id_dst.mkdir(exist_ok=True)
+            wav_path = constants.JOURJINE_ET_AL_2023_DATA / f"development{species_id}" / f"development_{species_id}" / source_file
+            if wav_path.exists():
+                simple_seq = crowsetta.formats.seq.SimpleSeq(
+                    onsets_s=this_file_segs_df.start_seconds.values,
+                    offsets_s=this_file_segs_df.stop_seconds.values,
+                    labels=np.array(['v'] * this_file_segs_df.stop_seconds.values.shape[-1]),
+                    annot_path='dummy',
+                )
+                if not dry_run:
+                    shutil.copy(wav_path, species_id_dst)
+                    csv_path = species_id_dst / f"{source_file}.call.csv"
+                    simple_seq.to_file(csv_path)
+
+
+def copy_wav_convert_annot_steinfath_et_al_2021_zebra_finch(dry_run=True):
+    """
+    """
+    logger.info(
+        f"Copying audio and annotation files for bird ID {constants.STEINFATH_ET_AL_2021_ZB_BIRD_ID} from Steinfath et al. 2021 dataset"
+    )
+    # only one zebra finch in the dataset,
+    # because it's so rare to have zebra finch data </s>
+    bird_id_dir = constants.STEINFATH_ET_AL_2021_RAW_DATA
+    assert bird_id_dir.exists(), f"Couldn't find bird_id_dir: {bird_id_dir}"
+
+    bird_id_dst = constants.ZB_DATA_DST / f"Steinfath-et-al-2021-{constants.STEINFATH_ET_AL_2021_ZB_BIRD_ID}"
+    bird_id_dst.mkdir(exist_ok=True)
+
+    # bfsongrepo is split up by days; we throw away that split here
+    logger.info(
+        f"Copying audio and annotation files from day: {bird_id_dir.name}"
+    )
+    wav_paths = sorted(bird_id_dir.glob("*wav"))
+    logger.info(
+        f"Found {len(wav_paths)} audio files."
+    )
+    csv_paths = sorted(bird_id_dir.glob("*csv"))
+    logger.info(
+        f"Found {len(csv_paths)} annotation files."
+    )
+    wav_paths_filtered = []
+    csv_paths_filtered = []
+    for wav_path in wav_paths:
+        csv_path_that_should_exist = wav_path.parent / (wav_path.stem + '_annotations.csv')
+        if csv_path_that_should_exist in csv_paths:
+            df = pd.read_csv(csv_path_that_should_exist)
+            if len(df) > 0:
+                # at least one annotation has no annotated segments in it; we skip
+                wav_paths_filtered.append(wav_path)
+                csv_paths_filtered.append(csv_path_that_should_exist)
+    logger.info(
+        "After filtering audio files by whether or not they have a valid annotation file, "
+        f"there are {len(wav_paths_filtered)} audio files and {len(csv_paths_filtered)} annotation files."
+    )
+    pbar = tqdm.tqdm(
+        zip(wav_paths_filtered, csv_paths_filtered)
+    )
+    for wav_path, csv_path in pbar:
+        pbar.set_description(
+            f"Copying wav + converting annotation for: {wav_path.name}"
+        )
+        if not dry_run:
+            shutil.copy(wav_path, bird_id_dst)
+        # NOTE we rename to include the unit in the annotation file name
+        simple_seq = crowsetta.formats.seq.SimpleSeq.from_file(
+            csv_path,
+            columns_map={
+                'start_seconds': 'onset_s', 'stop_seconds': 'offset_s', 'name': 'label'
+            },
+        )
+        csv_path_dst = bird_id_dst / (wav_path.name + '.syllable.csv')
+        if not dry_run:
+            simple_seq.to_file(csv_path_dst)
+
+
+def copy_audio_convert_annotations_timit(dry_run=True):
+    """"""
+    logger.info(
+        "Processing data from TIMIT, NLTK Sample"
+    )
+    TIMIT_NLTK_DATA_DIRS = [
+        subdir for subdir in constants.TIMIT_NLTK_RAW.iterdir()
+        if subdir.is_dir() and subdir.name.startswith('dr')
+    ]
+
+    # keys will be dialect region,
+    # values will be list of speaker IDs
+    nltk_dr_spkr_map = defaultdict(list)
+
+    for data_dir in TIMIT_NLTK_DATA_DIRS:
+        dir_name_upper = data_dir.name.upper()
+        dialect_region, speaker_id = dir_name_upper.split('-')
+        logger.info(
+            f"Processing data for dialect region '{dialect_region}', "
+            f"speaker ID: {speaker_id}"
+        )
+        nltk_dr_spkr_map[dialect_region].append(speaker_id)
+
+        wav_paths = sorted(data_dir.glob('*wav'))
+        wrd_paths = sorted(data_dir.glob('*wrd'))
+        phn_paths = sorted(data_dir.glob('*phn'))
+        if not len(wav_paths) == len(wrd_paths) == len(phn_paths):
+            raise ValueError(
+                f"len(wav_paths)={len(wav_paths)} != len(wrd_paths)={len(wrd_paths)} "
+                f"!= len(phn_paths)={len(phn_paths)}"
+            )
+
+        dst = constants.SPEECH_DATA_DST / f"TIMIT-NLTK-{dialect_region}-{speaker_id}"
+        dst.mkdir(exist_ok=True)
+        pbar = tqdm.tqdm(zip(
+            wav_paths, wrd_paths, phn_paths
+        ))
+        for wav_path, wrd_path, phn_path in pbar:
+            pbar.set_description(
+                f"Processing audio and annotations for: {wav_path.name}"
+            )
+            if not dry_run:
+                shutil.copy(wav_path, dst)
+
+            phn_seq = crowsetta.formats.seq.Timit.from_file(phn_path).to_seq()
+            phn_simpleseq = crowsetta.formats.seq.SimpleSeq(
+                onsets_s=phn_seq.onsets_s,
+                offsets_s=phn_seq.offsets_s,
+                labels=phn_seq.labels,
+                annot_path='dummy',
+            )
+            phn_csv_dst = dst / f"{wav_path.name}.phoneme.csv"
+            phn_simpleseq.to_file(phn_csv_dst)
+
+            wrd_seq = crowsetta.formats.seq.Timit.from_file(wrd_path).to_seq()
+            wrd_simpleseq = crowsetta.formats.seq.SimpleSeq(
+                onsets_s=wrd_seq.onsets_s,
+                offsets_s=wrd_seq.offsets_s,
+                labels=wrd_seq.labels,
+                annot_path='dummy',
+            )
+            wrd_csv_dst = dst / f"{wav_path.name}.word.csv"
+            wrd_simpleseq.to_file(wrd_csv_dst)
+
+    TIMIT_FULL_CORPUS_DATA_DIRS = sorted(constants.TIMIT_FULL_CORPUS_RAW.glob("T*/DR*/*/"))
+
+    logger.info(
+        "Processing data from TIMIT, full corpus"
+    )
+
+    n_skipped = 0
+    for data_dir in TIMIT_FULL_CORPUS_DATA_DIRS:
+        speaker_id = data_dir.name
+        dialect_region = data_dir.parents[0].name
+        if speaker_id in nltk_dr_spkr_map[dialect_region]:
+            logger.info(
+                f"Skipping speaker {speaker_id} because they are in NLTK TIMIT corpus sample"
+            )
+            continue
+        logger.info(
+            f"Processing data for dialect region '{dialect_region}', "
+            f"speaker ID: {speaker_id}"
+        )
+
+        wav_paths = sorted(data_dir.glob('*wav'))
+        wrd_paths = sorted(data_dir.glob('*WRD'))
+        phn_paths = sorted(data_dir.glob('*PHN'))
+        if not len(wav_paths) == len(wrd_paths) == len(phn_paths):
+            raise ValueError(
+                f"len(wav_paths)={len(wav_paths)} != len(wrd_paths)={len(wrd_paths)} "
+                f"!= len(phn_paths)={len(phn_paths)}"
+            )
+
+        dst = constants.HUMAN_SPEECH_WE_CANT_SHARE / f"TIMIT-full-corpus-{dialect_region}-{speaker_id}"
+        dst.mkdir(exist_ok=True)
+        pbar = tqdm.tqdm(zip(
+            wav_paths, wrd_paths, phn_paths
+        ))
+        for wav_path, wrd_path, phn_path in pbar:
+            pbar.set_description(
+                f"Processing audio and annotations for: {wav_path.name}"
+            )
+            wav_path_dst = dst / wav_path.stem.replace('.WAV', '.wav')
+            if not dry_run:
+                shutil.copy(wav_path, wav_path_dst)
+
+            phn_seq = crowsetta.formats.seq.Timit.from_file(phn_path).to_seq()
+            phn_simpleseq = crowsetta.formats.seq.SimpleSeq(
+                onsets_s=phn_seq.onsets_s,
+                offsets_s=phn_seq.offsets_s,
+                labels=phn_seq.labels,
+                annot_path='dummy',
+            )
+            phn_csv_dst = dst / f"{wav_path_dst.name}.phoneme.csv"
+            if not dry_run:
+                phn_simpleseq.to_file(phn_csv_dst)
+
+            wrd_seq = crowsetta.formats.seq.Timit.from_file(wrd_path).to_seq()
+            wrd_simpleseq = crowsetta.formats.seq.SimpleSeq(
+                onsets_s=wrd_seq.onsets_s,
+                offsets_s=wrd_seq.offsets_s,
+                labels=wrd_seq.labels,
+                annot_path='dummy',
+            )
+            wrd_csv_dst = dst / f"{wav_path_dst.name}.word.csv"
+            if not dry_run:
+                wrd_simpleseq.to_file(wrd_csv_dst)
+
+
 def copy_audio_copy_make_annot_all(dry_run=True):
     # ---- Bengalese finch song
     logger.info(
@@ -286,20 +525,20 @@ def copy_audio_copy_make_annot_all(dry_run=True):
     )
     copy_wav_convert_annot_tweetynet_canary(dry_run)
 
-    # ---- Zebra finch song
-    logger.info(
-        f"Getting audio and annotations for canary song."
-    )
-    copy_wav_convert_annot_tweetynet_canary(dry_run)
-
     # ---- Mouse pup calls
     logger.info(
-        f"Getting audio and annotations for canary song."
+        f"Getting audio and annotations for mouse pup song."
     )
-    copy_wav_convert_annot_tweetynet_canary(dry_run)
+    copy_wav_generate_annot_jourjine_et_al_2023(dry_run)
+
+    # ---- Zebra finch song
+    logger.info(
+        f"Getting audio and annotations for zebra finch song."
+    )
+    copy_wav_convert_annot_steinfath_et_al_2021_zebra_finch(dry_run)
 
     # ---- Human speech
     logger.info(
-        f"Getting audio and annotations for canary song."
+        f"Getting audio and annotations for human speech."
     )
-    copy_wav_convert_annot_tweetynet_canary(dry_run)
+    copy_audio_convert_annotations_timit(dry_run)
