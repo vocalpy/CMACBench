83ce854d413cd9becefcf37663a2cc310be60157
David Nicholson <nickledave@users.noreply.github.com>
2024-04-30 14:17:18 -0400

    Rewrite item transforms in src/biosoundsegbench/transforms/item.py

--- src/biosoundsegbench/transforms/item.py
+++ src/biosoundsegbench/transforms/item.py
@@ -13,14 +13,18 @@ import vak
 from .transforms import FrameLabelsMultiToBinary, FrameLabelsToBoundaryOnehot, ViewAsWindowBatch
 
 
+TARGET_TYPES = (
+    'boundary_onehot',
+    'multi_frame_labels',
+    'binary_frame_labels',
+)
+
+
 class TrainItemTransform:
-    """Transform used when training frame classification models"""
+    """Transform used when training models"""
     def __init__(
         self,
-        target_type: str,
         spect_standardizer=None,
-        labelmap: Mapping | None = None,
-        bg_class_name: str = 'unlabeled',
     ):
         if spect_standardizer is not None:
             if isinstance(spect_standardizer, vak.transforms.StandardizeSpect):
@@ -42,54 +46,13 @@ class TrainItemTransform:
         self.frames_transform = torchvision.transforms.Compose(
             frames_transform
         )
+        self.frame_labels_transform = vak.transforms.ToLongTensor()
 
-        if target_type == 'boundary':
-            self.to_boundary_onehot = torchvision.transforms.Compose([
-                FrameLabelsToBoundaryOnehot(),
-                vak.transforms.ToLongTensor(),
-            ])
-            self.frame_labels_transform = None
-        elif target_type == 'label-binary':
-            if labelmap is None:
-                raise ValueError(
-                    "`target_type` was 'label-binary' but `labelmap` was None. "
-                    "Must pass in a `labelmap` with an `unlabeled` class to convert "
-                    "multi-class labels to binary labels"
-                )
-            self.frame_labels_transform = torchvision.transforms.Compose([
-                FrameLabelsMultiToBinary(labelmap=labelmap, bg_class_name=bg_class_name),
-                vak.transforms.ToLongTensor(),
-            ])
-            self.to_boundary_onehot = None
-        elif target_type == 'label-multi':
-            self.frame_labels_transform = vak.transforms.ToLongTensor()
-            self.to_boundary_onehot = None
-        elif target_type == ('boundary', 'label-multi'):
-            self.frame_labels_transform = vak.transforms.ToLongTensor()
-            self.to_boundary_onehot = torchvision.transforms.Compose([
-                FrameLabelsToBoundaryOnehot(),
-                vak.transforms.ToLongTensor(),
-            ])
-        else:
-            raise ValueError(
-                f"Invalid `target_type`: {target_type}. "
-                "Must be one of: {'boundary', 'label-binary', 'label-multi', ('boundary', 'label-multi')}"
-            )
-
-        self.target_type = target_type
-        self.labelmap = labelmap
-        self.bg_class_name = bg_class_name
-
-    def __call__(self, frames, frame_labels):
-        frames = self.frames_transform(frames)
-        item = {
-            "frames": frames,
-        }
-        if self.frame_labels_transform is not None:
-            item["frame_labels"] = self.frame_labels_transform(frame_labels)
-        if self.to_boundary_onehot is not None:
-            item["boundary_onehot"] = self.to_boundary_onehot(frame_labels)
-
+    def __call__(self, item: dict):
+        item['frames'] = self.frames_transform(item['frames'])
+        for target_type in TARGET_TYPES:
+            if target_type in item:
+                item[target_type] = self.frame_labels_transform(item[target_type])
         return item
 
 
@@ -115,19 +78,11 @@ class EvalItemTransform:
                 )
         self.spect_standardizer = spect_standardizer
 
-        self.source_transform = torchvision.transforms.Compose(
-            [
-                vak.transforms.ToFloatTensor(),
-                # for frames we add channel at dim 0, not 1
-                vak.transforms.AddChannel(),
-            ]
-        )
-
         self.pad_to_window = vak.transforms.PadToWindow(
-            self.window_size, frames_padval, return_padding_mask=return_padding_mask
+            window_size, frames_padval, return_padding_mask=return_padding_mask
         )
 
-        self.transform_after_pad = torchvision.transforms.Compose(
+        self.frames_transform_after_pad = torchvision.transforms.Compose(
             [
                 vak.transforms.ViewAsWindowBatch(self.window_size),
                 vak.transforms.ToFloatTensor(),
@@ -143,36 +98,35 @@ class EvalItemTransform:
                     self.window_size, self.frame_labels_padval, return_padding_mask=False
                 ),
                 ViewAsWindowBatch(window_size),
+                vak.transforms.ToLongTensor()
             ]
         )
-        self.frame_labels_to_boundary_onehot = FrameLabelsToBoundaryOnehot()
 
-    def __call__(self, frames, frame_labels, frame_times, annot=None, frames_path=None):
+    def __call__(self, item, frame_times=None, annot=None, frames_path=None):
         # ---- handle spectrogram
         if self.spect_standardizer:
-            frames = self.spect_standardizer(frames)
+            frames = self.spect_standardizer(item['frames'])
+        else:
+            frames = item['frames']
 
         if self.pad_to_window.return_padding_mask:
-            frames_window, padding_mask = self.pad_to_window(frames)
+            frames, padding_mask = self.pad_to_window(frames)
         else:
-            frames_window = self.pad_to_window(frames)
+            frames = self.pad_to_window(frames)
             padding_mask = None
-        frames_window = self.transform_after_pad(frames_window)
 
-        # ---- handle frame labels
-        boundary_onehot = self.frame_labels_to_boundary_onehot(frame_labels)
-        boundary_onehot = self.frame_labels_transform(boundary_onehot)
-
-        item = {
-            "frames": frames_window,
-            "boundary_onehot": boundary_onehot,
-            "frame_times": frame_times,
-            "annot": annot,
-        }
-
+        item['frames'] = self.frames_transform_after_pad(frames)
         if padding_mask is not None:
             item["padding_mask"] = padding_mask
 
+        for target_type in TARGET_TYPES:
+            if target_type in item:
+                item[target_type] = self.frame_labels_transform(item[target_type])
+
+        if frame_times:
+            item['frame_times'] = frame_times
+        if annot:
+            item['annot'] = annot
         if frames_path is not None:
             # make sure frames_path is a str, not a pathlib.Path
             item["frames_path"] = str(frames_path)
